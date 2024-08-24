0728557c3958a9e76b2e6cc99d9d89b7acdcb283
Alkoun-web-design <113122406+Alkoun-web-design@users.noreply.github.com>
2024-08-25 02:56:44 +0500

    commented out MYSQL data references

--- src/pages/test.json.js
+++ src/pages/test.json.js
@@ -1,13 +1,14 @@
 // src/pages/api/test-db-connection.js
-import { testConnection } from '../../lib/db';
 
-export async function get({ params, request }) {
-  const result = await testConnection();
+// import { testConnection } from '../../lib/db';
+
+// export async function get({ params, request }) {
+//   const result = await testConnection();
   
-  return new Response(JSON.stringify(result), {
-    status: result.success ? 200 : 500,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
-}
+//   return new Response(JSON.stringify(result), {
+//     status: result.success ? 200 : 500,
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   });
+// }
