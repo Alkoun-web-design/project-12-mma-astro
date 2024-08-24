f50464170cdeee8981f7c0cb2362c740df0b048d
Alkoun-web-design <113122406+Alkoun-web-design@users.noreply.github.com>
2024-08-25 02:50:59 +0500

    changes in data.json.js

--- src/pages/data.json.js
+++ src/pages/data.json.js
@@ -1,23 +1,57 @@
-import { db, Tutors, like, eq, gt, count, sql } from 'astro:db';
-
 export async function GET({ request }) {
-  const url = new URL(request.url);
-  // const subjects = url.searchParams.get('subjects');
+ 
+const data = [
+  {
+    firstName:"Hisham",
+    lastName:"Rashdi",
+    age:"35",
+    avatar:"/avatars/man-1.webp",
+    subjects:["Maths","Physics"], 
+  },{
+    firstName:"Hasaan",
+    lastName:"Rashdi",
+    age:"21",
+    avatar:"/avatars/man-2.webp",
+    subjects:["English","Chemistry"]
+  },{
+    firstName:"Hassam",
+    lastName:"Sajid",
+    age:"36",
+    avatar:"/avatars/man-3.webp",
+    subjects:["Web Development"]
+  },{
+    firstName:"John",
+    lastName:"Cena",
+    age:"48",
+    avatar:"/avatars/john-cena.webp",
+    subjects:["Thugonomics", "Physical Education"]
+  },{
+    firstName:"Keith",
+    lastName:"Jardine",
+    age:"46",
+    avatar:"/avatars/keith-jardine.webp",
+    subjects:["English"]
+  },{
+    firstName:"Rich",
+    lastName:"Franklin",
+    age:"48",
+    avatar:"/avatars/rich-franklin.webp",
+    subjects:["Maths", "Physics"]
+  },{
+    firstName:"Myles",
+    lastName:"Kennedy",
+    age:"48",
+    avatar:"/avatars/myles-kennedy.webp",
+    subjects:["Music", "Vocals"]
+  }
+];
+  // const results = await query;
 
-  // This line produces the result of the bottom query
-  // let query = db.select( {schedule: Tutors.schedule} ).from(Tutors);
-  let tutorsdb = db.select().from(Tutors);
-  let query = tutorsdb.where(eq(Tutors.charges, 3000));
-  // if (subjects) {This line is the criteria someone will search for
-    // query = query.where(eq(Tutors.firstname, "Hassam"));
-  // }
-
-  const results = await query;
-
-  return new Response(JSON.stringify(results), {
+  return new Response(JSON.stringify(data), {
     status: 200,
     headers: {
       'Content-Type': 'application/json'
     }
+
   });
 }
